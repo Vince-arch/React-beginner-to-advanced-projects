8ae4eed097c3c734d43f3cf6b67f07bcab32e21b
Vince-arch <vincekimani006@gmail.com>
2023-12-02 16:11:13 +0300

    adding filter fuctionality to searchbar

--- 8.Note-Taking-App/src/App.jsx
+++ 8.Note-Taking-App/src/App.jsx
@@ -1,21 +1,27 @@
+import React, { useState } from 'react';
 import Header from "./Components/Header";
 import SearchBar from "./Components/SearchBar";
-import NotesList from './Components/NotesList'
-import { NoteProvider } from "./Components/NoteContext";
-import { useState } from "react";
+import NotesList from './Components/NotesList';
+import { NoteProvider, useNoteContext } from "./Components/NoteContext";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
 
-const [searchTerm, setSearchTerm] = useState('');
+  return (
+    <div name='container' className='pl-2 pr-2 flex flex-col m-auto'>
+      <Header />
+      <SearchBar searchNote={searchTerm} handleSearchNote={setSearchTerm} />
+      <NotesList searchTerm={searchTerm} />
+    </div>
+  );
+}
 
-
- return(
- <div name= 'container' className=" pl-2 pr-2 flex flex-col m-auto ">
+function AppWithProvider() {
+  return (
     <NoteProvider>
-    <Header />
-    <SearchBar  searchNote={searchTerm} handleSearchNote={setSearchTerm}/>
-    <NotesList />
+      <App />
     </NoteProvider>
- </div>
- )}
-export default App
+  );
+}
+
+export default AppWithProvider;
--- 8.Note-Taking-App/src/Components/NotesList.jsx
+++ 8.Note-Taking-App/src/Components/NotesList.jsx
@@ -2,26 +2,28 @@ import AddNote from './AddNote';
 import Note from './Note'
 import { useNoteContext } from './NoteContext';
 
+function NotesList({ searchTerm }) {
+  const { notes, addNote, deleteNote } = useNoteContext();
+  const handleDeleteNote = deleteNote;
 
-function NotesList(){
-   const { notes, addNote, setNotes, deleteNote} = useNoteContext();
-  
-   const handleDeleteNote = deleteNote
+  const filteredNotes = notes.filter((note) =>
+    note.text.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-return(
-<div name='notes-container' className='grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-3 '>
-   {notes.map((note)=> (
-      <Note 
-      id={note.id}
-      key={note.id} 
-      text={note.text} 
-      date={note.date}
-      handleDeleteNote={handleDeleteNote}
-      />
-   ))}
-   
-   <AddNote handleAddNote={addNote}/>
- </div>
-)}
+  return (
+    <div name='notes-container' className='grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-3 '>
+      {filteredNotes.map((note) => (
+        <Note
+          id={note.id}
+          key={note.id}
+          text={note.text}
+          date={note.date}
+          handleDeleteNote={handleDeleteNote}
+        />
+      ))}
+      <AddNote handleAddNote={addNote} />
+    </div>
+  );
+}
 
 export default NotesList;
